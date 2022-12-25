0bf10d338b999120d9f9b0e0d1fba87c6dc98b02
Michael Wu <wuxiaohua1011@gmail.com>
2022-12-25 15:13:24 -0800

    node manager launch file param added

--- launch/costmap_node_manager.launch.py
+++ launch/costmap_node_manager.launch.py
@@ -4,12 +4,19 @@ from ament_index_python.packages import get_package_share_directory
 import launch_ros
 from pathlib import Path
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition  # 1
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     base_path = Path(get_package_share_directory("costmap_node_manager"))
     default_costmap_config_file_path: Path = base_path / "config" / "config.yaml"
 
+    costmap_config_file_path_arg = launch.actions.DeclareLaunchArgument(
+        name="costmap_config_file_path",
+        default_value=default_costmap_config_file_path.as_posix(),
+    )
     costmap_manager = Node(
         executable="costmap_node_manager",
         package="costmap_node_manager",
@@ -18,13 +25,29 @@ def generate_launch_description():
         ],
     )
 
+    should_launch_local_costmap_marker_args = DeclareLaunchArgument(
+        "should_launch_local_costmap_marker",
+        default_value="False",  # default_value=[], has the same problem
+        description="true to start emitting local costmap detected obstacle markers. False by default",
+    )
+    local_costmap_marker = Node(
+        package="nav2_costmap_2d",
+        executable="nav2_costmap_2d_markers",
+        remappings=[
+            ("voxel_grid", "/local_costmap/voxel_grid"),
+            ("visualization_marker", "/local_costmap/voxel_grid/visualize"),
+        ],
+        condition=IfCondition(
+            LaunchConfiguration("should_launch_local_costmap_marker")
+        ),
+    )
     ld = launch.LaunchDescription(
         [
-            launch.actions.DeclareLaunchArgument(
-                name="costmap_config_file_path",
-                default_value=default_costmap_config_file_path.as_posix(),
-            ),
+            # args
+            costmap_config_file_path_arg,
+            should_launch_local_costmap_marker_args,
             costmap_manager,
+            local_costmap_marker,
         ]
     )
     return ld
