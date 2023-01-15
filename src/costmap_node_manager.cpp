ec363ca582a853a6683774d4e2d52f925e541e93
Michael Wu <wuxiaohua1011@gmail.com>
2023-01-15 15:14:55 -0800

    lite: global costmap optional

--- src/costmap_node_manager.cpp
+++ src/costmap_node_manager.cpp
@@ -18,18 +18,25 @@ namespace costmap_node_manager
     RCLCPP_INFO(get_logger(), "Creating Costmap Manager Node");
     local_costmap_ros_ = std::make_shared<nav2_costmap_2d::Costmap2DROS>(
         "local_costmap", std::string{get_namespace()}, "local_costmap");
+    
+    this->declare_parameter("should_activate_global_costmap", false);
     local_costmap_thread_ = std::make_unique<nav2_util::NodeThread>(local_costmap_ros_);
-
-    global_costmap_ros_ = std::make_shared<nav2_costmap_2d::Costmap2DROS>(
-        "global_costmap", std::string{get_namespace()}, "global_costmap");
-    global_costmap_thread_ = std::make_unique<nav2_util::NodeThread>(global_costmap_ros_);
+    if (this->get_parameter("should_activate_global_costmap").as_bool())
+    {
+      global_costmap_ros_ = std::make_shared<nav2_costmap_2d::Costmap2DROS>(
+          "global_costmap", std::string{get_namespace()}, "global_costmap");
+      global_costmap_thread_ = std::make_unique<nav2_util::NodeThread>(global_costmap_ros_);
+    }
   }
 
   CostmapNodeManager::~CostmapNodeManager()
   {
     RCLCPP_INFO(get_logger(), "Destroying ROS2Costmap2DNode");
     local_costmap_thread_.reset();
-    global_costmap_thread_.reset();
+    if (global_costmap_thread_)
+    {
+      global_costmap_thread_.reset();
+    }
   }
 
   nav2_util::CallbackReturn
@@ -37,7 +44,11 @@ namespace costmap_node_manager
   {
     RCLCPP_INFO(get_logger(), "Configuring");
     local_costmap_ros_->configure();
-    global_costmap_ros_->configure();
+    if (global_costmap_ros_)
+    {
+      global_costmap_ros_->configure();
+    }
+
     return nav2_util::CallbackReturn::SUCCESS;
   }
 
@@ -46,7 +57,10 @@ namespace costmap_node_manager
   {
     RCLCPP_INFO(get_logger(), "Activating");
     local_costmap_ros_->activate();
-    global_costmap_ros_->activate();
+    if (global_costmap_ros_)
+    {
+          global_costmap_ros_->activate();
+    }
     return nav2_util::CallbackReturn::SUCCESS;
   }
 
@@ -55,7 +69,11 @@ namespace costmap_node_manager
   {
     RCLCPP_INFO(get_logger(), "Deactivating");
     local_costmap_ros_->deactivate();
-    global_costmap_ros_->deactivate();
+    if (global_costmap_ros_)
+    {
+      global_costmap_ros_->deactivate();
+    }
+
     return nav2_util::CallbackReturn::SUCCESS;
   }
 
@@ -64,7 +82,11 @@ namespace costmap_node_manager
   {
     RCLCPP_INFO(get_logger(), "Cleaning up");
     local_costmap_ros_->cleanup();
-    global_costmap_ros_->cleanup();
+    if (global_costmap_ros_)
+    {
+      global_costmap_ros_->cleanup();
+    }
+
     return nav2_util::CallbackReturn::SUCCESS;
   }
 
@@ -73,7 +95,10 @@ namespace costmap_node_manager
   {
     RCLCPP_INFO(get_logger(), "Shutting Down");
     local_costmap_ros_->shutdown();
-    global_costmap_ros_->shutdown();
+    if (global_costmap_ros_)
+    {
+      global_costmap_ros_->shutdown();
+    }
     return nav2_util::CallbackReturn::SUCCESS;
   }
 }
